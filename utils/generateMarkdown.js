75c904c33db51f0ffb108b022b239fe7db32344d
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-03-15 23:08:45 -0700

    update to generatemarkdown js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,10 +11,33 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(userInput, userInfo) {
+	return (
+`# ${userInput.title}
+## Description
+${userInput.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Developer](#developer)
+## Installation
+${userInput.installation}
+## Usage
+${userInput.usage}
+## License
+![userLicense](https://img.shields.io/badge/license-${(userInput.license).replace(/ /g, "_")}-blue)
+## Contributing
+${userInput.contributing}
+## Tests
+${userInput.tests}
+## Developer
+![userImage](${userInfo.avatar_url})
+GitHub Username: [@${userInfo.login}](https://github.com/${userInfo.login})
+GitHub Email Address: [${userInfo.email}](mailto:${userInfo.email})`
+	);
 }
 
 module.exports = generateMarkdown;
