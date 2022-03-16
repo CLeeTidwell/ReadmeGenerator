e2e53ee54c00df8171a5fc74bc36585cd78fdd45
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-03-15 23:56:03 -0700

    update to index js

--- index.js
+++ index.js
@@ -6,57 +6,53 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
-  {
-    type: "input",
-    message: "What is your project Title?",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "What is a short description?",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "What are the steps required to install",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "Provide instructions and examples",
-    name: "usage",
-  },
-  {
-    type: "input",
-    message: "List the collaborators, such as links, profiles, tutorials, APIs",
-    name: "credits",
-  },
-  {
-    type: "list",
-    message: "What kind of license do you want?",
-    name: "license",
-    choices: ["MIT", "APACHE", "ISC", "NONE"],
-  },
-  {
-    type: "input",
-    message: "List all the features",
-    name: "features",
-  },
-  {
-    type: "input",
-    message: "How to contribute",
-    name: "contributor",
-  },
-  {
-    type: "input",
-    message: "What is your GitHub user name",
-    name: "username",
-  },
-  {
-    type: "input",
-    message: "What is your email",
-    name: "email",
-  }
+    {
+        type: 'input',
+        message: 'What is the application name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'Tell me about your application',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'How do I use the application?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What are the guidelines for contributing to the application?',
+        name: 'contributions'
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for the application?',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: 'What type of license will you use?',
+        name: 'license',
+        choices: ['Apache', 'BSD', 'MIT', 'Mozilla', 'none'],
+        default: 'MIT'
+    },
+    {
+        type: 'input',
+        message: 'What is your e-mail address?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github'
+    },
 ];
 
 // TODO: Create a function to write README file
