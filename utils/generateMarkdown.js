c5172044eb616b082ef1e38a1a7c0fb67afdebd8
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-03-15 23:55:07 -0700

    update to markdown js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,43 +1,64 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  if (license === 'Apache') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === 'BSD') {
+    return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  } else if (license === 'MIT') {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else if (license === 'Mozilla') {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  } else {
+    return ""
+  }
+};
 
-// TODO: Create a function that returns the license section of README
+//  function that creates the license link in the table of contents
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license === 'none') {
+    return ""
+  } else {
+    return `* [License](#license)`
+  }
+};
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(userInput, userInfo) {
-	return (
-`# ${userInput.title}
+// function that creates the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === 'none') {
+    return ""
+  } else {
+    return `## License
+${renderLicenseBadge(license)}`
+  }
+}
+
+// generates markdown for README
+function generateMarkdown(data) {
+  return `# ${data.name}
 ## Description
-${userInput.description}
+${data.description}
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Developer](#developer)
+* [Testing](#testing)
+* [Questions](#questions)
+${renderLicenseLink(data.license)}
 ## Installation
-${userInput.installation}
+${data.installation}
 ## Usage
-${userInput.usage}
-## License
-![userLicense](https://img.shields.io/badge/license-${(userInput.license).replace(/ /g, "_")}-blue)
-## Contributing
-${userInput.contributing}
-## Tests
-${userInput.tests}
-## Developer
-![userImage](${userInfo.avatar_url})
-GitHub Username: [@${userInfo.login}](https://github.com/${userInfo.login})
-GitHub Email Address: [${userInfo.email}](mailto:${userInfo.email})`
-	);
-}
+${data.usage}
+## Contributions
+${data.contributions}
+## Testing
+${data.test}
+${renderLicenseSection(data.license)}
+---
+### Questions
+${data.email}
+[${data.github}](https://github.com/${data.github})
+`;
+};
 
 module.exports = generateMarkdown;
